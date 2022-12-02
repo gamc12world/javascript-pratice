0f73608718d360c9de49381ec139139b8c148eac
gamc12world <113699703+gamc12world@users.noreply.github.com>
2022-12-02 11:46:50 +0545

    Create function.js

---
+++ function.js
@@ -0,0 +1,545 @@
+asdfg();
+
+function gh(name) {
+  name = "aman";
+  if (name === "aman") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let name = "AMane";
+
+function Ansd() {
+  let name = "AManer";
+  console.log(name);
+}
+
+console.log(name);
+Ansd();
+
+function Ams(arred, _Aer, _eryy) {
+  arred = ["Amansr", "ASDFGHJK"];
+  Aer = ["Amansr", "ASDFGHJK"];
+  eryy = "Amansr";
+  if (arred.length <= 0) {
+    arred.lenght <= 0;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function ispurple(color) {
+//   if (color === "purple") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// }
+
+function isPurple(color, bg, bh) {
+  color = "purple";
+  bg = "purple";
+  bh = "purple";
+  if (color || (bg && bh)) return "ertrue";
+  return false;
+}
+function isP(color) {
+  return color === "purple";
+}
+
+function psp(color) {
+  color = [1, 2, 34, 5, 5, 5, 5];
+  for (let x = 0; x < color.length; x++) {
+    console.log(color, [x]);
+  }
+  return color;
+}
+
+function typr(name, rollno) {
+  name = "Aman";
+  rollno = 1;
+  return name === "Aman" || rollno == 4;
+}
+
+const c = typr();
+
+// Let's talk about scope block in function//
+
+function Anime() {
+  var anime = "Not your type";
+  console.log(anime);
+}
+if (true) {
+  var t = "amas";
+  console.log(t);
+}
+
+console.log(t);
+
+function tye() {
+  var aniemw = "amsod";
+  console.log(aniemw);
+}
+
+for (let x = 0; x <= 5; x++) {
+  var s = "amsdd";
+  console.log(s, [x]);
+}
+console.log(s);
+
+var u = 0;
+while (u <= 4) {
+  u++;
+  console.log(u, "he;;p");
+}
+
+console.log(u);
+
+function doubel(arr) {
+  for (const num of arr) {
+    num = arr + 2;
+    num;
+  }
+}
+
+let f = ["Bear", "tiger", "lion"];
+
+for (var x = 0; x < f.length; x++) {
+  console.log(f, [x]);
+}
+console.log(x);
+
+if (true) {
+  var animal = "eele";
+  console.log(animal);
+}
+
+console.log(animal);
+
+const uio = function (a, b) {
+  tyr = a * b;
+  console.log(tyr);
+  return a * b;
+};
+
+// const math = {
+//   factit: function factorial(n) {
+//     console.log(n)
+//     if (n <= 1) {
+//       return 1;
+//     }
+//     return n * factorial(n - 1);
+//   }
+// };
+
+// math.factit(3)
+
+let pot = "Ams";
+console.log(pot);
+
+function typesc(r, t, d) {
+  let tyr = console.log(r + t + (d * 7) / 2);
+  return tyr;
+}
+
+const ty = {
+  donotsomething: typesc,
+};
+
+function callofduty(fg) {
+  fg();
+  fg();
+  fg();
+  fg();
+  fg();
+}
+
+function ds() {
+  console.log("Hey body");
+}
+
+callofduty(ds);
+
+function call(sd, x, c, v) {
+  for (let s = 0; s < v; s++) {
+    c();
+  }
+  sd = "Hellp";
+  x = "Jerry";
+  if (sd === "Hellp") {
+    console.log(true);
+  }
+  if (x === "Jerry") {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+call();
+call(ds, 2);
+function naw() {
+  console.log("you are");
+}
+
+function cr() {
+  console.log("CUte");
+}
+
+function good(s, sd, f) {
+  for (let x = 0; x < f; x++) {
+    `${s() + sd()`${f}`}`;
+  }
+}
+
+good(naw + cr, 112);
+
+function bad() {
+  console.log("good");
+}
+function ba() {
+  console.log("bad");
+}
+
+function god(s, u, i) {
+  s = 0;
+  while (s < 9) {
+    u();
+    i();
+    s++;
+  }
+}
+god(122, bad, ba);
+
+function aserd(p, a) {
+  if (p) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+aserd();
+
+function gets() {
+  return function (d) {
+    return d * 34;
+  };
+}
+
+const ypesc = gets();
+
+function ofa3b3() {
+  return function (x, d, e) {
+    x = 9;
+    d = 42;
+    e = 12;
+    return x === 9 || (d === 4 && e === 1);
+  };
+}
+
+function ofa2() {
+  return function (a) {
+    a = [1, 21, 23, 3, 3, 3, 3, 3, 3];
+    for (let x = 0; x < a.lenght; x++) {
+      console.log(a, [x]);
+    }
+  };
+}
+
+function convetofindtonpr() {
+  return function (x) {
+    let y = 1.6;
+    return x * y;
+  };
+}
+
+let ac = 0;
+while (ac <= 10) {
+  console.log("swarnim publication pvt ltd");
+  ac++;
+}
+
+function surface() {
+  return function (v, u, t) {
+    let a;
+    console.log((a = v - u / t));
+  };
+}
+
+// function rtyf() {
+//   alert("hello")
+// }
+//  const btn=document.getElementById('button');
+// btn.addEventListener('click me', rtyf)
+
+// setTimeout(function () {
+//   alert("Very good");
+// }, 2000);
+
+// console.log(
+//   setTimeout(function () {
+//     alert("hey goole");
+//   }),
+//   2334
+// );
+
+const rert = document.getElementById("e3");
+rert.addEventListener("click", function () {
+  let cv = prompt("neterrerre");
+  if (cv) {
+    alert("sucessfull registrer");
+  } else {
+    alert("not register");
+  }
+});
+var qwerty;
+console.log(qwerty);
+var qwerty = "aman";
+
+function asdfg() {
+  console.log("Ballllllllllllll", `${qwerty}`);
+}
+
+const books = [
+  {
+    ame: "AMAN",
+    class: "Ffdsdfsfsd",
+  },
+  {
+    ame: "ANIKET",
+    class: "fsdfd",
+  },
+  {
+    ame: "SHUVAM",
+    class: "fdsfsdf",
+  },
+];
+
+books.forEach(function (am) {
+  console.log(am.ame.toLowerCase());
+  console.log(am.class.toUpperCase());
+});
+
+const num = [1, 2, 3, 4, 8, 9, 0];
+
+num.forEach(function (n) {
+  console.log(n * 4);
+});
+const dublw = num.map(function (n) {
+  return n * 3;
+});
+
+const tripee = books.map(function (as) {
+  return as.ame.toLowerCase().split("").join("#");
+});
+console.log(tripee);
+
+const areaofrect = (length, breadth) => {
+  return length * breadth;
+};
+
+const nume = (sd) => {
+  return sd % 2 === 0;
+};
+
+const bookse = [
+  {
+    booss: "             link",
+    author: "IR simkhada",
+  },
+  {
+    booss: "        swarnim",
+    author: "shrestha",
+  },
+  {
+    booss: "                 relance",
+    author: "gopal",
+  },
+];
+
+bookse.forEach(function (tyhe) {
+  console.log(tyhe.author.toUpperCase());
+  //  console.log(tyhe.booss.toUpperCase());
+  console.log(tyhe.booss.trim().toUpperCase());
+});
+
+const doujb = [12, 67, 6678, 6787677];
+
+doujb.forEach(function (boid) {
+  console.log(boid * 2);
+});
+
+const fgh = [
+  {
+    bose: "dsfsdfd",
+    fact: {
+      anime: "rg bucket lis",
+      adsds: { asdw: "Dasdasdsd" },
+    },
+  },
+];
+
+fgh.forEach(function (ertyd) {
+  console.log(ertyd.fact.adsds.asdw);
+  console.log(ertyd.fact.adsds.asdw.toUpperCase());
+});
+
+const ddfe = (ddf, rrty) => {
+  console.log(ddf * rrty);
+};
+
+function callsw(f) {
+  for (let fe = 0; fe <= 3; fe++) {
+    console.log(f, fe);
+  }
+}
+
+const rage = () => {
+  console.log("Happy");
+};
+
+const bookst = [
+  {
+    title: "maths",
+  },
+  {
+    title: "science",
+  },
+  {
+    title: "optinal maths",
+  },
+];
+
+const ttiel = bookst.map((bs) => {
+  return bs.title.toUpperCase();
+});
+
+const dutre = [1, 2, 36, 7, 8, 6, 76, 7, 7, 66, 6, 676];
+
+const doubl = dutre.map((odd) => {
+  return odd % 4 === 0;
+});
+
+const lsfs = [
+  {
+    firstname: "aman",
+    lastname: "gupta",
+  },
+  { firstname: "aniket", lastname: "gupta" },
+  {
+    firstname: "anish",
+    lastname: "gupta",
+  },
+];
+
+const lase = lsfs.map(function (eks) {
+  return `${eks.firstname} ${eks.lastname}`;
+});
+
+const rt = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+const rte = rt.map((rtd) => {
+  return rtd % 4 === 1;
+});
+
+const dfdsf = [
+  {
+    tilte: "mad",
+    author: "aman",
+  },
+  {
+    tilte: "sfdsad",
+    author: "Fsdf",
+  },
+  {
+    tilte: "fsf",
+    author: "dsafs",
+  },
+];
+
+const tgh = dfdsf.map((rty, ty) => {
+  console.log(rty.author.toUpperCase());
+});
+
+const htu = [1, 2, 34, 8, 2, 2, 2, 1, 1, 12, 2, 3, 3, 3];
+
+const dubc = htu.map((n) => (n % 2 === 0 ? "e" : "o"));
+
+const dubc2 = htu.map((n) => (n % 2 === 0 ? "odd" : "even"));
+
+const dubc3 = htu.map((n) => {
+  n % 2 === 0;
+  if (n < 2) {
+    return "odd";
+  }
+  return "even";
+});
+
+let movies = [
+  "The Fantastic Mr. Fox",
+  "Mr. and Mrs. Smith",
+  "Mrs. Doubtfire",
+  "Mr. Deeds",
+];
+
+// Find first movie that includes 'Mrs' in its title:
+const movie = movies.find((movie) => {
+  return movie.includes("Mrs");
+});
+
+// const bsooks = [{
+//   title: 'Good Omens',
+//   authors: ['Terry Pratchett', 'Neil Gaiman'],
+//   rating: 4.25
+// },
+// {
+//   title: 'Bone: The Complete Edition',
+//   authors: ['Jeff Smith'],
+//   rating: 4.42
+// },
+// {
+//   title: 'American Gods',
+//   authors: ['Neil Gaiman'],
+//   rating: 4.11
+// },
+// {
+//   title: 'A Gentleman in Moscow',
+//   authors: ['Amor Towles'],
+//   rating: 4.36
+// }
+// ]
+// //Find first book w/ rating over 4.3
+// const goodBook = books.find(b => b.rating >= 4.3);
+
+// //Find first book with Neil Gaiman as an author:
+// const neilBook = bsooks.find(b => (
+// b.authors.includes('Neil Gaiman')
+// ))
+
+const bod = [
+  {
+    title: "abc",
+    author: "xyz",
+  },
+  {
+    title: "def",
+    author: "ghi.d",
+  },
+  {
+    title: "jkl",
+    author: "mno",
+  },
+];
+
+const type = bod.find((r) => {
+  return r.author.includes("i");
+});
+
+const de = ["a", "dasdb", "c", "d"];
+
+const hjkghfd = de.find((e) => {
+  return e.includes("b");
+});
